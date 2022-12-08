ff9b97fcf60483ff1d243c50ee8c111daf71356b
antoniobigas <antoniobigas@gmail.com>
2022-12-08 14:37:58 -0300

    Add Board

--- Jogo-Xadrez/src/boardgame/Piece.java
+++ Jogo-Xadrez/src/boardgame/Piece.java
@@ -1,4 +1,18 @@
 package boardgame;
 
-public class Piece {
+public class Piece extends Board {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
 }
+
